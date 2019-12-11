436c84034b688b34a061268f39fd3139e1e95eb5
zarim <zmuhammad55@gmail.com>
2019-12-11 00:26:46 -0500

    added in site screen shots and links

--- swe-project-site/src/App.js
+++ swe-project-site/src/App.js
@@ -2,11 +2,21 @@ import React, { Component} from "react";
 // import ScrollUpButton from "react-scroll-up-button";
 import logo from './images/placeholder.png';
 import zari from './images/zari.JPG';
-import jaida from 'realjaida2.jpg';
-import lelia from 'reallelia2.jpg';
-import margie from 'realMargie.jpg';
-import ulunma from 'realUlunma.JPG';
+import jaida from './images/realjaida2.jpg';
+import lelia from './images/reallelia2.jpg';
+import margie from './images/realMargie.jpg';
+import ulunma from './images/realUlunma.JPG';
+import job from './images/add-job-role.png';
+import blog from './images/blog.png';
+import candidate from './images/candidate-results.png';
+import expanded from './images/expanded-candidate-results.png';
+import login from './images/login.png';
+import missionPage from './images/mission-page.png';
+import mission from './images/mission.png';
+import salary from './images/salary-negotiation.png';
 import picture from './images/picture.png';
+import signup from './images/signup.png';
+import report from './pdf/Project-Report-3.pdf';
 import screenshot from './images/screenshot.jpg';
 import "./App.css";
 
@@ -28,7 +38,7 @@ class App extends Component{
             <h3 className="text" id="section-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
             <h2 className="text" id="section-subtitle">System Requirements</h2>
             <h3 className="text" id="section-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
-            <h2 className="text" id="section-subtitle">Link to Project Report</h2>
+            <h2 className="text" id="section-subtitle"><a href={report} target="_blank">Link to Project Report</a></h2>
         </div>
         <div className="team" id="team">
             <h1 className="text" id="section-title">Meet the Team</h1>
@@ -77,23 +87,23 @@ class App extends Component{
             <div className="left">
                 <h2 className="text" id="section-subtitle">To Do</h2>
                 <ul className="not-complete text" id="section-content">
-                    <li>To do item 1</li>
-                    <li>To do item 2</li>
-                    <li>To do item 3</li>
-                    <li>To do item 4</li>
-                    <li>To do item 5</li>
-                    <li>To do item 6</li>
+                    <li>Favorite a Job</li>
+                    <li>Contact Candidate</li>
+                    <li>Apply for Job</li>
+                    <li>Upload resumes</li>
+                    <li>Upload user profile picture</li>
+                    <li>Upload company logo</li>
                 </ul>
             </div>
             <div className="right">
                 <h2 className="text" id="section-subtitle">Done</h2>
                 <ul className="complete text" id="section-content">
-                    <li>Completed item 1</li>
-                    <li>Completed item 2</li>
-                    <li>Completed item 3</li>
-                    <li>Completed item 4</li>
-                    <li>Completed item 5</li>
-                    <li>Completed item 6</li>
+                    <li>Candidate/Company Signup</li>
+                    <li>Candidate/Company Login</li>
+                    <li>Job Search</li>
+                    <li>User/Candidate Profile</li>
+                    <li>Add Job Role</li>
+                    <li>Expanded Results</li>
                 </ul>
             </div>
         </div>
@@ -103,18 +113,23 @@ class App extends Component{
                 <h3 className="text" id="section-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
             </div>
             <div className="row1">
-                <img src={screenshot} id="screenshot" alt="screenshot placeholder"/>
-                <img src={screenshot} id="screenshot" alt="screenshot placeholder"/>
-                <img src={screenshot} id="screenshot" alt="screenshot placeholder"/>
+                <img src={missionPage} id="screenshot" alt="screenshot placeholder"/>
+                <img src={mission} id="screenshot" alt="screenshot placeholder"/>
+                <img src={blog} id="screenshot" alt="screenshot placeholder"/>
             </div>
             <div className="row1">
-                <img src={screenshot} id="screenshot" alt="screenshot placeholder"/>
-                <img src={screenshot} id="screenshot" alt="screenshot placeholder"/>
-                <img src={screenshot} id="screenshot" alt="screenshot placeholder"/>
+                <img src={salary} id="screenshot" alt="screenshot placeholder"/>
+                <img src={job} id="screenshot" alt="screenshot placeholder"/>
+                <img src={expanded} id="screenshot" alt="screenshot placeholder"/>
+            </div>
+            <div className="row1">
+                <img src={candidate} id="screenshot" alt="screenshot placeholder"/>
+                <img src={login} id="screenshot" alt="screenshot placeholder"/>
+                <img src={signup} id="screenshot" alt="screenshot placeholder"/>
             </div>
             <div className="column">
-                <h1 className="text" id="section-subtitle">Link to Project Site</h1>
-                <h1 className="text" id="section-subtitle">Link to Project Github</h1>
+                <h1 className="text" id="section-subtitle"><a href="https://glassceilingswe.firebaseapp.com/index.html" target="_blank">Link to Project Site</a></h1>
+                <h1 className="text" id="section-subtitle"><a href="https://github.com/zarim/JobPostingProject" target="_blank">Link to Project Github</a></h1>
             </div>
         </div>
       </div>
